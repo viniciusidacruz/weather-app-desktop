330542606abd063ef3ca6a9b7fb40272f0a4c8a9
viniciusidacruz <viniciusidacruz@hotmail.com>
2025-02-05 01:51:49 -0300

    chore: :wrench: Implement storybook for library components
    Implement storybook for library components

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,10 @@
-export const App = () => <h1>Hello</h1>;
+import { MapPin, Search } from "lucide-react";
+import { Input } from "./components/internal";
+
+export const App = () => (
+  <Input.Root>
+    <Input.Icon icon={MapPin} />
+    <Input.Field type="text" placeholder="Digite aqui" />
+    <Input.Icon icon={Search} />
+  </Input.Root>
+);
